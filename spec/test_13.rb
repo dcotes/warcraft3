02d86f7d7fba46370d427c17f50910fc730e6025
Dakota Carter <dakotacar@gmail.com>
2015-03-11 01:03:21 +0000

    cleaned up test 13 comments

--- spec/test_13.rb
+++ spec/test_13.rb
@@ -22,28 +22,4 @@ describe Unit do
       expect(@unit.attack!(enemy_unit)).to be_falsey
     end
   end
-
-  # describe "#attack!" do
-  #   it "attacks the enemy if both of them are alive" do
-  #     enemy_unit = Unit.new(10, 5)
-  #     enemy_unit.should_receive(:dead?).and_return(false)
-  #     @unit.should_receive(:dead?).and_return(false)
-  #     expect(@unit.attack!(enemy_unit)).to be_false
-  #   end
-  # end
-
-  # describe "#attack!" do 
-  #   it "cannot attack a dead enemy" do
-  #     enemy.should_receive(:dead?).and_return(true)
-  #     expect(@unit.attack!). to be_false
-  #   end
-  # end
-
-  # describe "#attack!" do
-  #   it "cannot attack a dead enemy" do
-  #     enemy_unit = Unit.new(0, 5)
-  #     enemy_unit.should_receive(:dead?).and_return(true)
-  #     expect(@unit.attack!(enemy_unit)).to be_false
-  #   end
-  
 end
